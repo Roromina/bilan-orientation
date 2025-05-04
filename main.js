7e2dd513a3938345c1687011112814cc5413227f
Romy Seube <romyseube@Air-de-Romy.fritz.box>
2025-05-04 17:03:38 +1100

    test JS

---
+++ main.js
@@ -0,0 +1,35 @@
+console.log("JS chargé");
+document.getElementById('bilan-form').addEventListener('submit', function (e) {
+    e.preventDefault();
+  
+    const form = e.target;
+  
+    const data = {
+      nom: form.nom.value,
+      q1: form.q1.value,
+      q2: form.q2.value,
+      q20: "", // à compléter plus tard
+      metier: form.metier.value
+    };
+  
+    fetch("https://script.google.com/macros/s/AKfycbxiweWCp9wdfS8qCFLBwao-xjEVIKz2sypgDGDDKk_jTxHjbh8qwb0MEx0v4uyn5WHP/exec", {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+    .then(response => {
+      if (response.ok) {
+        document.getElementById('confirmation').style.display = 'block';
+        form.reset();
+      } else {
+        alert("Une erreur est survenue. Réessaye.");
+      }
+    })
+    .catch(error => {
+      console.error(error);
+      alert("Erreur de connexion. Vérifie ta connexion Internet.");
+    });
+  });
+  

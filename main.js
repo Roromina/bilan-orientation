075ab3138b3f534ab91bc4868239e1919bb8fba7
Romy Seube <romyseube@Air-de-Romy.fritz.box>
2025-05-04 18:03:43 +1100

    Passage à GET pour contourner CORS

--- main.js
+++ main.js
@@ -1,41 +1,38 @@
 console.log("JS chargé");
+
 document.getElementById('bilan-form').addEventListener('submit', function (e) {
-    e.preventDefault();
-  
-    const form = e.target;
-  
-    const data = {
-        nom: form.nom.value,
-        q1: form.q1.value,
-        q2: form.q2.value,
-        q3: form.q3.value,
-        q4: form.q4.value,
-        q5: form.q5.value,
-        q6: form.q6.value,
-        q7: form.q7.value,
-        q8: form.q8.value,
-        q9: form.q9.value,
-        q10: form.q10.value,
-        q11: Array.from(form.querySelectorAll('input[name="q11"]:checked')).map(e => e.value).join(", "),
-        q13: form.q13.value,
-        q14: form.q14.value,
-        q15: form.q15.value,
-        q16: form.q16.value,
-        q17: form.q17.value,
-        q18: form.q18.value,
-        q19: form.q19.value,
-        q20: form.q20.value,
-        metier: form.metier.value
-      };
-      
-  
-    fetch("https://script.google.com/macros/s/AKfycbw79hmXATVh38hOhiQxDRWIbyy6w6kyPsb1zeX9HgEeLf_4zLwVsOZZenMpEIVENS6x/exec", {
-      method: "POST",
-      body: JSON.stringify(data),
-      headers: {
-        "Content-Type": "application/json"
-      }
-    })
+  e.preventDefault();
+
+  const form = e.target;
+
+  const data = {
+    nom: form.nom.value,
+    q1: form.q1.value,
+    q2: form.q2.value,
+    q3: form.q3.value,
+    q4: form.q4.value,
+    q5: form.q5.value,
+    q6: form.q6.value,
+    q7: form.q7.value,
+    q8: form.q8.value,
+    q9: form.q9.value,
+    q10: form.q10.value,
+    q11: Array.from(form.querySelectorAll('input[name="q11"]:checked')).map(e => e.value).join(", "),
+    q13: form.q13.value,
+    q14: form.q14.value,
+    q15: form.q15.value,
+    q16: form.q16.value,
+    q17: form.q17.value,
+    q18: form.q18.value,
+    q19: form.q19.value,
+    q20: form.q20.value,
+    metier: form.metier.value
+  };
+
+  const params = new URLSearchParams(data).toString();
+  const url = "https://script.google.com/macros/s/AKfycbyrQH6xXm4roGU-T32N2oi-2-7WoLkBvV_lQRsr2hD1aL86YOXKvMkJJMrXABgUgzdz/exec" + params;
+
+  fetch(url)
     .then(response => {
       if (response.ok) {
         document.getElementById('confirmation').style.display = 'block';
@@ -46,7 +43,6 @@ document.getElementById('bilan-form').addEventListener('submit', function (e) {
     })
     .catch(error => {
       console.error(error);
-      alert("Erreur de connexion. Vérifie ta connexion Internet.");
+      alert("Erreur de connexion.");
     });
-  });
-  
+});
